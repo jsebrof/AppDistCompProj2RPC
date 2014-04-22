d5e4c8183f937a1d1239b843bb577e07821dfc9d
Jonathan Forbes <jsebrof@gmail.com>
2014-04-22 16:55:26 -0700

    Setup java class files for the server and client

---
+++ src/RPCClient.java
@@ -0,0 +1,7 @@
+
+public class RPCClient {
+	public static void main(String args[]) throws Exception
+	{
+		
+	}
+}
---
+++ src/RPCServer.java
@@ -0,0 +1,7 @@
+
+public class RPCServer {
+	public static void main(String args[]) throws Exception
+	{
+		
+	}
+}
